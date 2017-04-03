31a8ef8fb77718e94194df1585f09de8e7824e82
benben1996 <benjaminsimode1996@gmail.com>
2017-04-03 09:43:21 +0200

    Modif vh px pour mobile

--- assets/js/pageloader.js
+++ assets/js/pageloader.js
@@ -48,6 +48,19 @@
         }
 
         function chargementTermine() {
+
+            function vhTOpx(value) {
+                var w = window,
+                    d = document,
+                    e = d.documentElement,
+                    g = d.getElementsByTagName('body')[0],
+                    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+                var result = (y*value)/100;
+                return(result);
+            }
+            var px = vhTOpx(100);
+
             var pourcentage = 100;
 
             // Affichage du pourcentage
