d3bd53ce62073fcfab0ae716b89818c5a09bad82
benben1996 <benjaminsimode1996@gmail.com>
2017-05-09 16:05:54 +0200

    Modif majeur page unique structure

--- assets/js/animation-gradient-accueil.js
+++ assets/js/animation-gradient-accueil.js
@@ -1,6 +1,38 @@
+var color1, color2;
+
 document.getElementById('bouton-exp').onclick = function(){
-    var color = '#';
-    var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0'];
-    color += letters[Math.floor(Math.random() * letters.length)];
-    document.getElementById('accueil').style.background = color;
+    var colors = [
+        '#ff2e46',
+        '#ff1945',
+        '#6ec4ff',
+        '#ff9d33',
+        '#a067ff'
+    ];
+
+    color1 = colors[Math.floor(Math.random() * colors.length)];
+    color2 = colors[Math.floor(Math.random() * colors.length)];
+
+    var divCircle = document.createElement('div');
+    divCircle.className = 'circle-experience';
+
+    if(document.getElementById('bloc-experience').childNodes.length >= 2){
+        document.getElementById('bloc-experience').removeChild(document.getElementById('bloc-experience').childNodes[0]);
+    }
+    prefixValue(divCircle, color1, color2);
+    document.getElementById('bloc-experience').appendChild(divCircle);
+    setTimeout(function(){
+        divCircle.style.transform = 'translate(50%, 50%) scale(1)';
+    }, 500);
+
+
 };
+
+function prefixValue(element, color1, color2) {
+    var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
+
+    console.log(element);
+    prefixes.forEach(function (prefixe){
+        element.style.background = prefixe + 'linear-gradient(230deg, ' + color1 + ', ' + color2 + ')';
+        console.log(prefixe, 'linear-gradient(230deg, ' + color1 + ', ' + color2 + ')');
+    });
+}
