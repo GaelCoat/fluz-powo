0624b42ee4d6fcfef6d637759d491d8ef5a41139
benben1996 <benjaminsimode1996@gmail.com>
2017-05-09 13:30:31 +0200

    Modif majeur page unique structure

--- assets/js/animation-gradient-accueil.js
+++ assets/js/animation-gradient-accueil.js
@@ -1,3 +1,6 @@
-$(document).ready(function (){
-
-});
+document.getElementById('bouton-exp').onclick = function(){
+    var color = '#';
+    var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0'];
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('accueil').style.background = color;
+};
