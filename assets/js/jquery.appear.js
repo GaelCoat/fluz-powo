8edb44e647de57c164da9c59051774e17c63656b
benben1996 <benjaminsimode1996@gmail.com>
2017-05-14 15:12:44 +0200

    Modif responsive

--- assets/js/jquery.appear.js
+++ assets/js/jquery.appear.js
@@ -10,67 +10,68 @@
      *     the user visible viewport of a web browser.
      *     only accounts for vertical position, not horizontal.
      */
+    if ($(window).width() > 768) {
+        $.fn.visible = function (partial) {
 
-    $.fn.visible = function (partial) {
+            var $t = $(this),
+                $w = $(window),
+                viewTop = $w.scrollTop(),
+                viewBottom = viewTop + $w.height(),
+                _top = $t.offset().top,
+                _bottom = _top + $t.height(),
+                compareTop = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
 
-        var $t = $(this),
-            $w = $(window),
-            viewTop = $w.scrollTop(),
-            viewBottom = viewTop + $w.height(),
-            _top = $t.offset().top,
-            _bottom = _top + $t.height(),
-            compareTop = partial === true ? _bottom : _top,
-            compareBottom = partial === true ? _top : _bottom;
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+        };
 
-    };
 
-})(jQuery);
+        var win = $(window);
 
-var win = $(window);
+        var allMods = $(".module");
 
-var allMods = $(".module");
+        allMods.each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("already-visible");
+            }
+        });
 
-allMods.each(function (i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-        el.addClass("already-visible");
+        win.scroll(function (event) {
+
+            allMods.each(function (i, el) {
+                var el = $(el);
+                if (el.visible(true)) {
+                    el.removeClass("module");
+                    el.addClass("come-in");
+                }
+                else {
+                    el.addClass("module");
+                    el.removeClass("come-in");
+                }
+            });
+        });
+
+
+        var alMods = $(".module-one-time");
+
+        alMods.each(function (i, ol) {
+            var ol = $(ol);
+            if (ol.visible(true)) {
+                ol.addClass("already-visible");
+            }
+        });
+
+        win.scroll(function (event) {
+
+            alMods.each(function (i, ol) {
+                var ol = $(ol);
+                if (ol.visible(true)) {
+                    ol.removeClass("module");
+                    ol.addClass("come-in");
+                }
+            });
+        });
     }
-});
-
-win.scroll(function (event) {
-
-    allMods.each(function (i, el) {
-        var el = $(el);
-        if (el.visible(true)) {
-            el.removeClass("module");
-            el.addClass("come-in");
-        }
-        else {
-            el.addClass("module");
-            el.removeClass("come-in");
-        }
-    });
-});
-
-
-var alMods = $(".module-one-time");
-
-alMods.each(function (i, ol) {
-    var ol = $(ol);
-    if (ol.visible(true)) {
-        ol.addClass("already-visible");
-    }
-});
-
-win.scroll(function (event) {
-
-    alMods.each(function (i, ol) {
-        var ol = $(ol);
-        if (ol.visible(true)) {
-            ol.removeClass("module");
-            ol.addClass("come-in");
-        }
-    });
 });
