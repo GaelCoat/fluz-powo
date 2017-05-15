b9e7db7d0bcc3f049e5fadc41541a70ff5cb1a66
benben1996 <benjaminsimode1996@gmail.com>
2017-05-15 12:36:59 +0200

    r

--- assets/js/jquery.appear.js
+++ assets/js/jquery.appear.js
@@ -1,38 +1,26 @@
 (function ($) {
+    $.fn.visible = function (partial) {
 
-    /**
-     * Copyright 2012, Digital Fusion
-     * Licensed under the MIT license.
-     * http://teamdf.com/jquery-plugins/license/
-     *
-     * @author Sam Sehnert
-     * @desc A small plugin that checks whether elements are within
-     *     the user visible viewport of a web browser.
-     *     only accounts for vertical position, not horizontal.
-     */
-    if ($(window).width() > 768) {
-        $.fn.visible = function (partial) {
+        var $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
 
-            var $t = $(this),
-                $w = $(window),
-                viewTop = $w.scrollTop(),
-                viewBottom = viewTop + $w.height(),
-                _top = $t.offset().top,
-                _bottom = _top + $t.height(),
-                compareTop = partial === true ? _bottom : _top,
-                compareBottom = partial === true ? _top : _bottom;
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
-            return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    };
 
-        };
-
-
+    if($(window).width() > 768){
         var win = $(window);
 
         var allMods = $(".module");
 
         allMods.each(function (i, el) {
-            var el = $(el);
+            el = $(el);
             if (el.visible(true)) {
                 el.addClass("already-visible");
             }
@@ -41,7 +29,7 @@
         win.scroll(function (event) {
 
             allMods.each(function (i, el) {
-                var el = $(el);
+                el = $(el);
                 if (el.visible(true)) {
                     el.removeClass("module");
                     el.addClass("come-in");
@@ -51,27 +39,27 @@
                     el.removeClass("come-in");
                 }
             });
-        });
 
 
-        var alMods = $(".module-one-time");
-
-        alMods.each(function (i, ol) {
-            var ol = $(ol);
-            if (ol.visible(true)) {
-                ol.addClass("already-visible");
-            }
-        });
-
-        win.scroll(function (event) {
+            var alMods = $(".module-one-time");
 
             alMods.each(function (i, ol) {
-                var ol = $(ol);
+                ol = $(ol);
                 if (ol.visible(true)) {
-                    ol.removeClass("module");
-                    ol.addClass("come-in");
+                    ol.addClass("already-visible");
                 }
+            });
+
+            win.scroll(function (event) {
+
+                alMods.each(function (i, ol) {
+                    ol = $(ol);
+                    if (ol.visible(true)) {
+                        ol.removeClass("module");
+                        ol.addClass("come-in");
+                    }
+                });
             });
         });
     }
-});
+})(jQuery);
